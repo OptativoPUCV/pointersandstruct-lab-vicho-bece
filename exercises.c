0a1e0205a166fe1cebc2e06c90c6118b9f67d370
Replit user <replituser@example.com>
2021-03-19 00:25:00 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -65,6 +65,7 @@ Persona* crearPersona(char nombre[], char rut[], int edad) {
   strcpy(p->nombre, nombre);
   strcpy(p->rut, rut);
   p->edad = edad;
+  printf("%d\n", p->edad);
    return NULL;
 }
 
